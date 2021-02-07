872b2bb559c7c1439095d2e7c7788602fe603dad
shivank-dev <sharmashivank59@gmail.com>
2021-02-07 20:14:30 +0530

    fix port issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require("cors");
-const dotenv = require("dotenv").config({path: '.env'});
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const AppRouter = require('./app/routes/AppRouter');
 
@@ -23,6 +23,6 @@ app.use(cors());
 new AppRouter(app).initRoutes();
 
 const server = app.listen(5000, () => {
-    const port = server.address().port;
+    const port = process.env.PORT;
     console.log("Server is listening on port %d in %s mode", port, app.settings.env);
 })
