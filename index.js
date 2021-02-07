f7f53758521e59cab6873eea3827766e91fcdf2c
shivank-dev <sharmashivank59@gmail.com>
2021-02-07 14:01:03 +0530

    db connection created

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config();
 const app = express();
+
+mongoose.set("useFindAndModify", false)
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("Connected to db");
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-
 app.use(cors());
 
 app.get('/ping', (req, res) => {
     res.send(new Date());
 });
+
 
 const server = app.listen(5000, () => {
     const port = server.address().port;
