03c2ec09160050e1aff9273765615e67b23fe613
Shivank Sharma <30679936+Shivank-Dev@users.noreply.github.com>
2021-02-07 15:39:50 +0530

    merge master into main

---
+++ index.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const AppRouter = require('./app/routes/AppRouter');
+
+dotenv.config();
+const app = express();
+
+mongoose.set("useFindAndModify", false)
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("Connected to db");
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(cors());
+
+new AppRouter(app).initRoutes();
+
+const server = app.listen(5000, () => {
+    const port = server.address().port;
+    console.log("Server is listening on port %d in %s mode", port, app.settings.env);
+})
