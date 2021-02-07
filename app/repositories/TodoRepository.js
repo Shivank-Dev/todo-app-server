c9024865f6cc5dd23910ff918120ccd919bdf1a2
Shivank Sharma <30679936+Shivank-Dev@users.noreply.github.com>
2021-02-07 15:38:18 +0530

    master merged into main

---
+++ app/repositories/TodoRepository.js
@@ -0,0 +1,43 @@
+const Todo = require('../models/Todo');
+
+class TodoRepository {
+
+    constructor(model) {
+        this.model = model;
+    }
+
+    create(name) {
+        const newTodo = {
+            name,
+            isDone: false
+        };
+        const todo = new this.model(newTodo);
+
+        return todo.save();
+    }
+
+    findAll() {
+        return this.model.find();
+    }
+
+    findById(id) {
+        return this.model.findById(id);
+    }
+
+    deleteById(id) {
+        return this.model.findByIdAndDelete(id);
+    }
+
+    updateById(id, object) {
+        const query = {
+            _id: id
+        };
+        return this.model.findOneAndUpdate(query, {
+            $set: {
+                name: object.name,
+                isDone: object.done
+            }
+        });
+    }
+}
+module.exports = new TodoRepository(Todo);
